920f0b4c69a5120c4f0607d752027d24ab69e4e0
jjvega86 <jj.vega86@gmail.com>
2020-09-08 12:24:46 -0400

    Problem 2 solved and tested

--- LINQ Problems/Program.cs
+++ LINQ Problems/Program.cs
@@ -10,6 +10,8 @@ namespace LINQ_Problems
     {
         static void Main(string[] args)
         {
+            // Problem 1: Using LINQ, write a function that returns all words that contain the substring “th” from a list
+
             List<string> words = new List<string>() { "the", "bike", "this",  "it", "tenth", "mathematics" };
             var wordsWithTh = words.Where(w => w.Contains("th"));
             foreach (var word in wordsWithTh)
@@ -17,6 +19,13 @@ namespace LINQ_Problems
                 Console.WriteLine(word);
             }
 
+            // Problem 2: Using LINQ, write a function that takes in a list of strings and returns a copy of the list without duplicates
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Zack", "Mike" };
+            var listWithoutDuplicates = names.Distinct().ToList();
+            foreach (var name in listWithoutDuplicates)
+            {
+                Console.WriteLine(name);
+            }
             Console.ReadLine();
 
         }
