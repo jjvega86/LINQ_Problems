0c24cfaee4a4c7f1663bde86d210ebee3e4a7696
jjvega86 <jj.vega86@gmail.com>
2020-09-10 09:30:53 -0400

    Finished and tested LINQ problem #3

--- LINQ Problems/Program.cs
+++ LINQ Problems/Program.cs
@@ -8,25 +8,53 @@ namespace LINQ_Problems
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             // Problem 1: Using LINQ, write a function that returns all words that contain the substring “th” from a list
 
-            List<string> words = new List<string>() { "the", "bike", "this",  "it", "tenth", "mathematics" };
-            var wordsWithTh = words.Where(w => w.Contains("th"));
-            foreach (var word in wordsWithTh)
-            {
-                Console.WriteLine(word);
-            }
+            //List<string> words = new List<string>() { "the", "bike", "this",  "it", "tenth", "mathematics" };
+            //var wordsWithTh = words.Where(w => w.Contains("th"));
+            //foreach (var word in wordsWithTh)
+            //{
+            //    Console.WriteLine(word);
+            //}
 
             // Problem 2: Using LINQ, write a function that takes in a list of strings and returns a copy of the list without duplicates
-            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Zack", "Mike" };
-            var listWithoutDuplicates = names.Distinct().ToList();
-            foreach (var name in listWithoutDuplicates)
-            {
-                Console.WriteLine(name);
+
+            //List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Zack", "Mike" };
+            //var listWithoutDuplicates = names.Distinct().ToList();
+            //foreach (var name in listWithoutDuplicates)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+            // Problem 3: Using LINQ, write a function that calculates the class grade average after dropping the lowest grade for each student. 
+            // The function should take in a list of strings of grades (e.g., one string might be "90,100,82,89,55"), 
+            // drops the lowest grade from each string, averages the rest of the grades from that string, then averages the averages.
+
+            List<string> classGrades = new List<string>()   {
+                                                            "80,100,92,89,65",
+                                                            "93,81,78,84,69",
+                                                            "73,88,83,99,64",
+                                                            "98,100,66,74,55"
+                                                            };
+
+            double cumulativeTotal = 0;
+            foreach (string s in classGrades)
+            {               
+                string[] grades = s.Split(','); // Split each string by comma before converting to ints
+                var gradesInt = grades.Select(g => Int32.Parse(g)).ToList(); // select each string in array, parse to int, add to new list
+                gradesInt.Remove(gradesInt.Min()); // remove lowest int from array
+                cumulativeTotal += gradesInt.Average(); // add the average of the shrunk array to a cumulative total
+                
             }
+
+            var finalTotal = cumulativeTotal/classGrades.Count; // find average of all averaged grades divided by total strings in original list
+
+
             Console.ReadLine();
+
 
         }
     }
